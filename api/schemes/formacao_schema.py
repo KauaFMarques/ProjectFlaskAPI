e5e3acadd85037cc72305d3178090c17e4b251e1
KauaFMarques <kauamarques52@outlook.com>
2024-07-09 22:22:34 -0300

    firstcommit main

--- api/schemes/formacao_schema.py
+++ api/schemes/formacao_schema.py
@@ -1,16 +1,25 @@
 from api import ma
 from ..models import formacao_model
 from marshmallow import fields
-from ..schemes import curso_schema,professor_schema
+from ..schemes import curso_schema, professor_schema
 
 
-class FormacaoSchema(ma.SQLAlchemyAutoSchema,):
-    professores=ma.Nested(professor_schema.ProfessorSchema,many=True,only=("id","nome"))
+class FormacaoSchema(ma.SQLAlchemyAutoSchema):
+    professores = ma.Nested(professor_schema.ProfessorSchema, many=True, only=('id', 'nome'))
+
     class Meta:
         model = formacao_model.Formacao
         load_instance = True
-        fields = ("id", "nome", "descricao", "cursos", "professores")
+        fields = ("id", "nome", "descricao", "cursos", "professores", "_links")
 
     nome = fields.String(required=True)
     descricao = fields.String(required=True)
     cursos = fields.List(fields.Nested("CursoSchema", only=('id', 'nome')))
+
+    _links = ma.Hyperlinks(
+        {
+            "get": ma.URLFor("formacaodetail", id="<id>"),
+            "put": ma.URLFor("formacaodetail", id="<id>"),
+            "delete": ma.URLFor("formacaodetail", id="<id>")
+        }
+    )
