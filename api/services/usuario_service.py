fd763db01b8345b3be5459bef90f85972ffe1f00
KauaFMarques <kauamarques52@outlook.com>
2024-08-02 19:49:10 -0300

    finish 87

--- api/services/usuario_service.py
+++ api/services/usuario_service.py
@@ -2,7 +2,7 @@ from ..models import usuario_model
 from api import db
 
 def cadastrar_usuario(usuario):
-    usuario_bd = usuario_model.Usuario(nome=usuario.nome, email=usuario.email, senha=usuario.senha,is_admin=usuario.is_admin)
+    usuario_bd = usuario_model.Usuario(nome=usuario.nome, email=usuario.email, senha=usuario.senha, is_admin=usuario.is_admin, api_key=usuario.api_key)
     usuario_bd.encriptar_senha()
     db.session.add(usuario_bd)
     db.session.commit()
@@ -12,4 +12,4 @@ def listar_usuario_email(email):
     return usuario_model.Usuario.query.filter_by(email=email).first()
 
 def listar_usuario_id(id):
-    return usuario_model.Usuario.query.filter_by(id=id).first()
+    return usuario_model.Usuario.query.filter_by(id=id).first
